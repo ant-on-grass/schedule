976adc5aafc76e5dc30c12bf67dabca441a479cc
TE <gimt75257@gmail.com>
2024-12-04 14:59:53 +0900

    #1

--- src/main/java/com/sparta/schedule/dto/ResponseDto.java
+++ src/main/java/com/sparta/schedule/dto/ResponseDto.java
@@ -11,6 +11,7 @@ public class ResponseDto {
     private String author;
     private String contents ;
 
+    public ResponseDto(){} //TODO 기본 생성자가 필요! massagecontroller 가 서버에 맞는 형태로 바꿔줄때에 필요하다.
 
     public ResponseDto(Scheduleitem scheduleitem){
 
