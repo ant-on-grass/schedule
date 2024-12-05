75dd506a707d64b2ce4cf6d4aa49a6e58cf3ce35
TE <gimt75257@gmail.com>
2024-12-05 22:03:01 +0900

    #3 fix: ScheduleRepository Class's schescheduleView and ScheduleViewService Class's scheduleView - Role Coordination

--- src/main/java/com/sparta/schedule/service/ScheduleViewService.java
+++ src/main/java/com/sparta/schedule/service/ScheduleViewService.java
@@ -7,7 +7,9 @@ import com.sparta.schedule.entity.Scheduleitem;
 import com.sparta.schedule.repository.ScheduleRepositoryJdbc;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -28,7 +30,6 @@ public class ScheduleViewService {
         //TODO repository 의 결과 값을 다시 로직으로 처리 할 수 있다
 
 
-
         //TODO 예로 scheduleViewAll 의 실행 로직
         //TODO 그후에 값을 controller 로 넘김
         return responseDtos;
@@ -37,7 +38,32 @@ public class ScheduleViewService {
 
     public ResponseDto scheduleView(ViewRequestDto dto) throws SQLException {
 
-        ResponseDto responseDtos = jdbc.scheduleView(dto);
-        return responseDtos;
+        ResultSet resultSet = jdbc.scheduleView(dto);
+
+        ResponseDto responseDto = new ResponseDto();
+        try (resultSet) {
+            if (resultSet.next()) {
+                responseDto.setId(resultSet.getLong("id"));
+                responseDto.setAuthor(resultSet.getString("author"));
+                responseDto.setContents(resultSet.getString("comments"));
+
+                Timestamp flexDatePreConvert = resultSet.getTimestamp("flexDate");
+                if (flexDatePreConvert != null) {
+                    responseDto.setFlexDate(flexDatePreConvert.toLocalDateTime());
+                }
+
+                Timestamp fixDatePreConvert = resultSet.getTimestamp("fixDate");
+                if (fixDatePreConvert != null) {
+                    responseDto.setFixDate(fixDatePreConvert.toLocalDateTime());
+
+                }
+                return responseDto;
+            } else {
+                // 데이터가 없을 경우
+                System.out.println("ID " + dto.getId() + "에 해당하는 데이터가 없습니다.");
+                return null; //TODO 혹은 Optional.empty()로 감싸기
+            }
+        }
     }
 }
+
